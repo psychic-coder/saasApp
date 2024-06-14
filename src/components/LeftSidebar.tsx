972e274e9f8cb749b32ca98f82f895a93fe52426
psychic-coder <rohitganguly450@gmail.com>
2024-06-14 15:41:34 +0530

    public folder imported again

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -1,18 +1,43 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-
+'use client';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { sidebarLinks } from "@/constants/index"
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const LeftSidebar = () => {
+  const pathname = usePathname();
   return (
     <section className="left_sidebar">
-        <nav className="flex flex-col gap-6">
-            <Link href="/" >
-            <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
-            </Link>
-        </nav>
-    </section>
-  )
-}
+      <nav className="flex flex-col gap-6">
+        <Link
+          href="/"
+          className="flex cursor-pointer items-center gap-1  pb-10 max-lg:justify-center"
+        >
+          <Image
+            src="/public/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="Logo"
+          />
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden">
+            Podcastr
+          </h1>
+        </Link>
+        {sidebarLinks.map(({ route, label, imgURL }) => {
+          const isActive = pathname === route || pathname.startsWith(`${route}/`);
 
-export default LeftSidebar
+          return <Link href={route} key={label} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", {
+            'bg-nav-focus border-r-4 border-orange-1': isActive
+          })}>
+            <Image src={imgURL} alt={label} width={24} height={24} />
+            <p>{label}</p>
+          </Link>
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export default LeftSidebar;
